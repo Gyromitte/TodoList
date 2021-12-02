64965152c2f7be1d61e965c5e2a063c05ed7f238
Gyromitte <gyromitte@gmail.com>
2021-12-01 18:31:30 -0600

    Translated staticPage html to javaScript

--- src/createStaticPage.js
+++ src/createStaticPage.js
@@ -5,5 +5,24 @@ function createStaticPage(){
     header.id = "header";
     header.innerText = "Todo List"
     content.appendChild(header);
+
+    const container = document.createElement('container');
+    content.appendChild(container);
+
+    const sideBar = document.createElement('sideBar');
+    sideBar.id = "sideBar";
+    sideBar.innerText = "Projects";
+    container.appendChild(sideBar);
+
+    const newProject = document.createElement('div');
+    newProject.classList = "sideBarElement";
+    newProject.innerText = "Add new Project +";
+    sideBar.appendChild(newProject);
+
+    const projectContainer = document.createElement('project-container');
+    container.appendChild(projectContainer);
+
+    const projectWindow = document.createElement('project-window');
+    projectContainer.appendChild(projectWindow);
 }
 export {createStaticPage};
